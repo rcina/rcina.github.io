34b6757c69246f7bf56e47c2610d1cd140352d42
Robert <robcina@gmail.com>
2023-12-23 13:42:20 -0500

    Add ability to add youtube links to org-mode files

--- build-site.el
+++ build-site.el
@@ -38,6 +38,27 @@
              :section-numbers nil ;; Don't include section numbers
              :time-stamp-file nil))) ;; Don't include time stamp in file
 
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
 ;; generate the site output
 (org-publish-all t)
 
